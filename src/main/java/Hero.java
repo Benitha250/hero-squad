07cb0b30bbf0a91052865ecf08e9824041c4a755
Benitha250 <benithauwase05@gmail.com>
2020-11-20 09:52:47 +0200

    add relevant information in hero and squad class

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,2 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Hero {
+    private String hName;
+    private int hAge;
+    private String hSpecialPower;
+    private String hWeakness;
+    private static List<Hero> instances = new ArrayList<>();
+    private int hId;
+    public Hero(String name, int age, String special_power,String weakness){
+        this.hName = name;
+        hAge = age;
+        this.hSpecialPower = special_power;
+        this.hWeakness = weakness;
+        instances.add(this);
+        hId = instances.size();
+    }
+    public String getName(){
+        return hName;
+    }
+    public int getAge(){
+        return hAge;
+    }
+    public String getSpecialPower(){
+        return hSpecialPower;
+    }
+    public String getWeakness(){
+        return hWeakness;
+    }
+    public int getId(){
+        return hId;
+    }
+    public static List<Hero> all(){
+        return instances;
+    }
+    public static void clear(){
+        instances.clear();
+    }
+    public static Hero find(int id){
+        return instances.get(id-1);
+    }
+    public static void remove(int id){
+        instances.remove(id-1);
+    }
 }
--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,2 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Squad {
+    private int maxSize;
+    private String name;
+    private String cause;
+    private static List<Squad> instances = new ArrayList<>();
+    private int id;
+    private List<Hero> heros;
+    public Squad(String name, int max_size, String cause) {
+        this.name = name;
+        this.maxSize = max_size;
+        this.cause = cause;
+        instances.add(this);
+        id = instances.size();
+        heros = new ArrayList<>();
+    }
+    public String getName(){
+        return name;
+    }
+    public int getSize(){
+        return maxSize;
+    }
+    public String getCause(){
+        return cause;
+    }
+    public static List<Squad> all(){
+        return instances;
+    }
+    public int getId(){
+        return id;
+    }
+    public static Squad find(int id){
+        return instances.get(id-1);
+    }
+    public static void clear(){
+        instances.clear();
+    }
+    public static void remove(int id){
+        instances.remove(id-1);
+        for (Squad squad:instances) {
+            squad.id = instances.indexOf(squad)+1;
+        }
+    }
+    public List<Hero> getHeros(){
+        return heros;
+    }
+    public void addHero(Hero myHero){
+        if (heros.size()<maxSize){
+            heros.add(myHero);
+        }
+    }
+    public void removeHero(Hero myHero){
+        heros.remove(myHero);
+    }
 }
